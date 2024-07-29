0445285d9ac64f5868de1c133de5d3de6f5e9957
João Sena <126720426+jS3na@users.noreply.github.com>
2024-07-29 17:55:00 -0300

    Update config.php

--- db/config.php
+++ db/config.php
@@ -7,7 +7,7 @@ $senha_bd = "admin";
 $banco = 'controle_mam';
 
 $conn = new mysqli($host, $login, $senha_bd, $banco);
-//verifica se a conexão foi estabelecida com sucesso
+
 if ($conn->connect_error) {
     die('deu erro irmao ' . $conn->connect_error);
 }
